7032afec422a158cc614f0e0baa42abed40c317c
Ruben Bakker <rubenbak@gmail.com>
2015-02-02 22:00:36 +0200

    Serve routes and static files using iron-defer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate iron;
+extern crate router;
+extern crate mount;
+extern crate "static" as static_file;
+extern crate "iron-defer" as defer;
+
+use iron::prelude::*;
+use iron::{status, AfterMiddleware};
+use router::Router;
+use mount::Mount;
+use static_file::Static;
+use defer::Defer;
+
 fn main() {
-    println!("Hello, world!");
+	fn hello_world(_: &mut Request) -> IronResult<Response> {
+		Ok(Response::with((status::Ok, "Hello world")))
+	}
+
+	fn about(_: &mut Request) -> IronResult<Response> {
+		Ok(Response::with((status::Ok, "About")))
+	}
+
+	let mut mount = Mount::new();
+
+	mount.mount("/", Static::new(Path::new(".")));
+
+	let mut router = Router::new();
+
+    router.get("/", hello_world);
+    router.get("/about", about);
+
+    let defer = Defer::using(router, mount);
+
+    Iron::new(defer).listen("localhost:3000").unwrap();
 }
