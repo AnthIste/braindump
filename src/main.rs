3d038e82cd0191b6dff896e50386cbdb94258e26
Ruben Bakker <rubenbak@gmail.com>
2015-02-11 22:10:25 +0200

    Add React demo as static content
    Currently helloworld.html is being served as text/plain and is not being rendered by the browser.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(path)]
+
 extern crate iron;
 extern crate router;
 extern crate mount;
@@ -5,13 +7,17 @@ extern crate "static" as static_file;
 extern crate "iron-defer" as defer;
 
 use iron::prelude::*;
-use iron::{status, AfterMiddleware};
+use iron::status;
 use router::Router;
 use mount::Mount;
 use static_file::Static;
 use defer::Defer;
 
 fn main() {
+	// ------------------------------------------------------
+	// Dynamic handlers
+	// ------------------------------------------------------
+
 	fn hello_world(_: &mut Request) -> IronResult<Response> {
 		Ok(Response::with((status::Ok, "Hello world")))
 	}
@@ -20,14 +26,27 @@ fn main() {
 		Ok(Response::with((status::Ok, "About")))
 	}
 
+	// ------------------------------------------------------
+	// Mount points
+	// ------------------------------------------------------
+
 	let mut mount = Mount::new();
 
-	mount.mount("/", Static::new(Path::new(".")));
+	mount.mount("/", Static::new(Path::new("content")));
+
+	// ------------------------------------------------------
+	// Routes
+	// ------------------------------------------------------
 
 	let mut router = Router::new();
 
-    router.get("/", hello_world);
+    router.get("/", Static::new(Path::new("content/helloworld.html")));
+    router.get("/hello", hello_world);
     router.get("/about", about);
+
+    // ------------------------------------------------------
+	// Combine routes + mount points
+	// ------------------------------------------------------
 
     let defer = Defer::using(router, mount);
 
